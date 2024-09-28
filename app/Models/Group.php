14225e47d49b0886f4eb3ed7b692aa5229101bd0
Exxon Mecnix <emecnix@gmail.com>
2024-09-28 13:46:27 +0100

    Added models and migrations

---
+++ app/Models/Group.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_id',
+        'last_message_id',
+
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'group_users');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
